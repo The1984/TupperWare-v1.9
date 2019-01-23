1fc6437a4612cab32df5fe8df32164a26d725dfa
Ramos Javier <aranibar.javier@gmail.com>
2019-01-23 09:40:59 -0300

    modifico validacion de cliente

--- src/main/java/presentacion/controlador/ControladorVentanaAgregarCliente.java
+++ src/main/java/presentacion/controlador/ControladorVentanaAgregarCliente.java
@@ -34,12 +34,9 @@ public class ControladorVentanaAgregarCliente implements SujetoObservable
 	public void registrarCliente()
 	{
 		if(ValidadorCampos.campoVacio(this.ventana.getTxtNombre().getText())||
-		   ValidadorCampos.campoVacio(this.ventana.getTxtApellido().getText())||
-		   ValidadorCampos.campoVacio(this.ventana.getTxtDireccion().getText())||
-		   ValidadorCampos.campoVacio(this.ventana.getTxtCelular().getText())||
-		   ValidadorCampos.campoVacio(this.ventana.getTxtEmail().getText()))
+		   ValidadorCampos.campoVacio(this.ventana.getTxtApellido().getText()))
 		{
-			JOptionPane.showMessageDialog(null, "¡Falta completar campos!", "Warning", JOptionPane.WARNING_MESSAGE);
+			JOptionPane.showMessageDialog(null, "¡Los campos nombre y apellido son obligatorios!", "Warning", JOptionPane.WARNING_MESSAGE);
 			return;
 		}
 		if(!ValidadorCampos.validarNombre(this.ventana.getTxtNombre().getText()))
@@ -52,25 +49,31 @@ public class ControladorVentanaAgregarCliente implements SujetoObservable
 			JOptionPane.showMessageDialog(null, "¡Formato de apellido invalido!", "Error", JOptionPane.ERROR_MESSAGE);
 			return;
 		}
-		if(!ValidadorCampos.validarCelular(this.ventana.getTxtCelular().getText()))
-		{
-			JOptionPane.showMessageDialog(null, "¡Formato de celular invalido!", "Error", JOptionPane.ERROR_MESSAGE);
-			return;			
-		}
-		if(!ValidadorCampos.validarMail(this.ventana.getTxtEmail().getText()))
-		{
-			JOptionPane.showMessageDialog(null, "¡Formato de e-mail invalido!", "Error", JOptionPane.ERROR_MESSAGE);
-			return;						
-		}
 		if(ValidadorLogico.existeNombreApellidoAgregar(this.ventana.getTxtNombre().getText(), this.ventana.getTxtApellido().getText(), GestorClientes.getInstance().readAll())) 
 		{
 			JOptionPane.showMessageDialog(null, "¡Nombre y Apellido ya existente!", "Warning", JOptionPane.WARNING_MESSAGE);
 			return;									
 		}
-		if(ValidadorLogico.existeEmailAgregar(this.ventana.getTxtEmail().getText().toLowerCase(), GestorClientes.getInstance().readAll())) 
+		if(!ValidadorCampos.campoVacio(this.ventana.getTxtCelular().getText()))
 		{
-			JOptionPane.showMessageDialog(null, "¡Email ya existente!", "Warning", JOptionPane.WARNING_MESSAGE);
-			return;									
+			if(!ValidadorCampos.validarCelular(this.ventana.getTxtCelular().getText()))
+			{
+				JOptionPane.showMessageDialog(null, "¡Formato de celular invalido!", "Error", JOptionPane.ERROR_MESSAGE);
+				return;			
+			}			
+		}
+		if(!ValidadorCampos.campoVacio(this.ventana.getTxtEmail().getText()))
+		{
+			if(!ValidadorCampos.validarMail(this.ventana.getTxtEmail().getText()))
+			{
+				JOptionPane.showMessageDialog(null, "¡Formato de e-mail invalido!", "Error", JOptionPane.ERROR_MESSAGE);
+				return;						
+			}
+			if(ValidadorLogico.existeEmailAgregar(this.ventana.getTxtEmail().getText().toLowerCase(), GestorClientes.getInstance().readAll())) 
+			{
+				JOptionPane.showMessageDialog(null, "¡Email ya existente!", "Warning", JOptionPane.WARNING_MESSAGE);
+				return;									
+			}
 		}
 		ClienteDTO newCliente = new ClienteDTO();
 		newCliente.setNombre(this.ventana.getTxtNombre().getText());
